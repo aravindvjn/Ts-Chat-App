927f81d0ad9df3c297a2831fb8281aaf25afa184
Aravind Vijayan <aravind284479@gmail.com>
2024-11-04 14:02:35 +0530

    Update Links.tsx

--- frontend/src/global/Links/Links.tsx
+++ frontend/src/global/Links/Links.tsx
@@ -2,4 +2,4 @@ export const friendURL = import.meta.env.VITE_APP_BASE_URL + "/friends/";
 export const authURL = import.meta.env.VITE_APP_BASE_URL + "/auth/";
 export const chatURL = import.meta.env.VITE_APP_BASE_URL + "/chat/";
 export const baseURL = import.meta.env.VITE_APP_BASE_URL;
-export const AppName = "Ts Chat App";
+export const AppName = "Chinnamma Chats";
