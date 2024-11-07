4161b62ffad13b567dd96dd986ed48f4bdd5362e
Aravind Vijayan <aravind284479@gmail.com>
2024-11-08 00:08:36 +0530

    Update SplashScreen.tsx

--- frontend/src/components/SplashScreen/SplashScreen.tsx
+++ frontend/src/components/SplashScreen/SplashScreen.tsx
@@ -1,7 +1,7 @@
 const SplashScreen = () => {
   return (
     <div className="bg-background justify-center flex items-center max-h-svh">
-      <p className="text-text">Ts Chat App</p>
+      <p className="text-black">Ts Chat App</p>
     </div>
   );
 };
