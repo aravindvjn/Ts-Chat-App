4b7b3b571e7f0ed8527511b95db8e6b264463c03
Aravind Vijayan <aravind284479@gmail.com>
2024-11-09 19:29:10 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,7 +33,7 @@ function App() {
     return <SplashScreen />;
   }
   return (
-    <div data-aos="slide-left" className="max-w-[640px] m-auto bg-background h-svh">
+    <div data-aos="slide-left" className="max-w-[640px] m-auto bg-background h-lvh">
       {userData?.notification && (
         <Notification
           notification={userData?.notification}
